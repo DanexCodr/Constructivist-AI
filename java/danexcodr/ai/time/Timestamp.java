de0730efd2a641f51f59761f70616d86e102d202
Danison Nuñez <danisonnunez001@gmail.com>
2026-03-13 19:23:30 +0800

    Add files via upload

--- java/danexcodr/ai/time/Timestamp.java
+++ java/danexcodr/ai/time/Timestamp.java
@@ -1,10 +1,5 @@
 package danexcodr.ai.time;
 
-/** * This will be used alongside BitDT
-    * in the future for persistent
-    * timestamping storage
-    * **/
-
 public interface Timestamp {
     long getCreatedAt();
     long getLastModifiedAt();
